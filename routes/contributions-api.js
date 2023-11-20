aef902d5aab0d2b91829fd93ddf5388f8e8ab3e9
dylanhamada <dylan.hamada@gmail.com>
2023-11-19 21:18:41 -0500

    Flesh Out Route Descriptions
    - Flesh out route descriptions on routes.md
    - Create new directory notes/ and move routes.md into it
    - Delete users.ejs

--- routes/contributions-api.js
+++ routes/contributions-api.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router  = express.Router();
 
 // Create new database entry for new contribution, then redirect to story page
-router.post('/new', (req, res) => {});
+router.post('/', (req, res) => {});
 
 // Adjust vote count in database for contribution with matching id
-router.post('/:id/like', (req, res) => {});
+router.patch('/:id/like', (req, res) => {});
 
 // Accept contribution with matching id and incorporate it into the main story, then redirect to '/stories/:id'
-router.post('/:id/accept', (req, res) => {});
+router.patch('/:id/accept', (req, res) => {});
 
 module.exports = router;
