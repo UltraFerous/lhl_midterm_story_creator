ddc4fbb07c82eb04c99438466d7eeff90ffc8cbc
dylanhamada <dylan.hamada@gmail.com>
2023-11-21 18:35:12 -0500

    Modify Seed Files With Realistic Data
    - Modify all seed files to include realistic data
    - Change data type of body column to TEXT in stories schema
    - Change data type of title column to TEXT in stories schema
    - Change data type of body column to TEXT in contributions schema
    - Remove UNIQUE flag for author_id and contribution_id in votes schema

--- db/seeds/05_votes.sql
+++ db/seeds/05_votes.sql
@@ -1,2 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO votes (author_id, contribution_id) VALUES (3, 2);
+INSERT INTO votes (author_id, contribution_id) VALUES
+(1, 1), (2, 1), (3, 2), (4, 3), (5, 5), (6, 3), (7, 4), (8, 6), (9, 5), (10, 4);
