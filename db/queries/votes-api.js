5d1a6d251b7d0b84027de7aaefdad5b9a10eccec
UltraFerous <jandoga31@gmail.com>
2023-11-22 13:27:18 -0500

    Added liked functionality

--- db/queries/votes-api.js
+++ db/queries/votes-api.js
@@ -10,4 +10,30 @@ const addVote = function(authorId, contributionId) {
     .catch(err => console.log("ERROR:", err.message));
 };
 
-module.exports = { addVote };
+const findVotes = function(userID, storyID) {
+  return db
+    .query(`
+    SELECT
+    votes.author_id,
+    contributions.story_id,
+    votes.contribution_id,
+    votes.status 
+        FROM contributions
+        JOIN users ON contributions.author_id = users.id
+        JOIN votes ON contributions.id = votes.contribution_id
+    WHERE 
+    votes.author_id = $1
+    AND
+    contributions.story_id = $2;
+    `, [userID, storyID])
+    .then( function(data)
+      {
+        return data.rows;
+      }
+    )
+    .catch(err => console.log("ERROR:", err.message));
+};
+
+
+
+module.exports = { addVote, findVotes };
